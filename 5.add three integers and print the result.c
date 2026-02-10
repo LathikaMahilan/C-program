0ac7eea16c8d10d48b67aba75542dbdd32d8e13c
LathikaMahilan <mlathikamlathika78@gmail.com>
2026-02-10 11:25:37 +0530

    Add files via upload

---
+++ 5.add three integers and print the result.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+int main(){
+    
+    int a,b,c,d,e,f,g,h,i;
+    scanf("%d %d %d %d %d %d %d %d %d",&a,&b,&c,&d,&e,&f,&g,&h,&i);
+    int m=a+b+c;
+    int n=d+e+f;
+    int o=g+h+i;
+    printf("%d\n",m);
+    printf("%d\n",n);
+    printf("%d\n",o);
+    
+    
+    return 0;     
+         
+}
