ccc9b09d49a70ce9c33f83ec33ea474b386c4027
LathikaMahilan <mlathikamlathika78@gmail.com>
2026-02-10 13:35:58 +0530

    Add files via upload

---
+++ 3.apply pre-decrement to a variable.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    a=10;
+    int b;
+    b=1;
+    int c;
+    c=5;
+    
+    printf("%d\n",--a);
+    printf("%d\n",--b);
+    printf("%d\n",--c);
+    
+    
+    return 0;
+}
