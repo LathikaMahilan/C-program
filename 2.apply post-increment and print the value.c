376640513c31b5963a1213bba338391a04fbe084
LathikaMahilan <mlathikamlathika78@gmail.com>
2026-02-10 13:31:44 +0530

    Add files via upload

---
+++ 2.apply post-increment and print the value.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    a=5;
+    int b;
+    b=9;
+    int c;
+    c=0;
+    
+    printf("%d\n",a++);
+    printf("%d\n",b++);
+    printf("%d\n",c++);
+    
+    
+    return 0;
+}
