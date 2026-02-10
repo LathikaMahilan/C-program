f1cd8c4f1e46c7e864c7d2e58c7be67e6de1b271
LathikaMahilan <mlathikamlathika78@gmail.com>
2026-02-10 11:14:11 +0530

    Add files via upload

---
+++ 4.print quotient and remainder or two integers.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+int main(){
+    
+    int a=9;
+    int b=2;
+    int c=10;
+    int d=3;
+    int e=20;
+    int f=4;
+ 
+    
+    printf("Quotient=%dremainder=%d\n",a/b,a%b);
+    printf("Quotient=%dRemainder=%d\n",c/d,c%d);
+    printf("Quotient=%dRemainder=%d",e/f,e%f);
+ 
+    
+    return 0;     
+         
+}
