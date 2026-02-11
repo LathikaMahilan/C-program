b700183fad6b1a41bfe569823c8a1d466fca5392
LathikaMahilan <mlathikamlathika78@gmail.com>
2026-02-11 09:31:02 +0530

    Add files via upload

---
+++ 1.compare greater .c.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+
+int main()
+{
+   int a;
+   a=10;
+   int b;
+   b=5;
+   int c;
+   c=3;
+   int d;
+   d=8;
+   int e;
+   e=7;
+   int f;
+   f=7;
+   
+   int  x=a>b;
+   int y=c>d;
+   int z=e>f;
+   
+   printf("%d\n",x);
+   printf("%d\n",y);
+   printf("%d",z);
+   
+    return 0;
+}
