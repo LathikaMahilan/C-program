ac023acdbf9fd4c79e65a34e43e7beccbd8cdd4b
LathikaMahilan <mlathikamlathika78@gmail.com>
2026-02-11 10:31:36 +0530

    Add files via upload

---
+++ 5.compare two numbers using greater than and less than.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+
+int main()
+{
+   int a;
+   a=5;
+   int b;
+   b=5;
+   int c;
+   c=10;
+   int d;
+   d=5;
+   int e;
+   e=3;
+   int f;
+   f=7;
+   
+   printf("%d ",a>=b);
+   printf("%d",a<=b);
+   printf("\n%d ",c>=d);
+   printf("%d",c<=d);
+   printf("\n%d ",e>=f);
+   printf("%d ",e<=f);
+   
+    
+    return 0;
+}
