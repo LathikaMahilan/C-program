6c4033d9b2fa12f8d2d91ea3caac66e224226fd8
LathikaMahilan <mlathikamlathika78@gmail.com>
2026-02-11 09:40:09 +0530

    Add files via upload

---
+++ 4.compare the number not equal.c.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+
+int main()
+{
+   int a;
+   a=5;
+   int b;
+   b=3;
+   int c;
+   c=4;
+   int d;
+   d=4;
+   int e;
+   e=9;
+   int f;
+   f=1;
+   
+   int  x=a!=b;
+   int y=c!=d;
+   int z=e!=f;
+   
+   printf("%d\n",x);
+   printf("%d\n",y);
+   printf("%d",z);
+   
+    return 0;
+}
