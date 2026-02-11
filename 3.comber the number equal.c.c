3cac0eb9ea9a9022ca4d36ebe2b3e03cc9b806e0
LathikaMahilan <mlathikamlathika78@gmail.com>
2026-02-11 09:36:27 +0530

    Add files via upload

---
+++ 3.comber the number equal.c.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+
+int main()
+{
+   int a;
+   a=5;
+   int b;
+   b=5;
+   int c;
+   c=3;
+   int d;
+   d=4;
+   int e;
+   e=0;
+   int f;
+   f=0;
+   
+   int  x=a==b;
+   int y=c==d;
+   int z=e==f;
+   
+   printf("%d\n",x);
+   printf("%d\n",y);
+   printf("%d",z);
+   
+    return 0;
+}
