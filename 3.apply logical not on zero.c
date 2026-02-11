caab2f9ec98ca341a145d90c733733ef7d83c38f
LathikaMahilan <mlathikamlathika78@gmail.com>
2026-02-11 10:43:20 +0530

    Add files via upload

---
+++ 3.apply logical not on zero.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    a=0;
+    int b;
+    b=0;
+    int c;
+    c=0;
+    
+    printf("%d",!a);
+    printf("\n%d",!b);
+    printf("\n%d",!c);
+    
+    return 0;
+}
