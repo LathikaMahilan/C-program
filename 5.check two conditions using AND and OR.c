13aff4efd61c4d2dc37b4707ac1080a1fc4707b5
LathikaMahilan <mlathikamlathika78@gmail.com>
2026-02-11 10:53:10 +0530

    Add files via upload

---
+++ 5.check two conditions using AND and OR.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+
+int main()
+{
+   int a;
+   a=5;
+   int b;
+   b=10;
+   int c;
+   c=0;
+   int d;
+   d=5;
+   int e;
+   e=0;
+   int f;
+   f=0;
+   
+   
+   printf("%d",a&&b & a||b);
+   printf("\n%d",c&&d & c||d);
+   printf("\n%d",e&&f & e||f);
+   
+   return 0;
+   
+}
