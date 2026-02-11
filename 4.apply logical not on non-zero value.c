f39263a81ceaf0a80687757805d67d6172c4ba7e
LathikaMahilan <mlathikamlathika78@gmail.com>
2026-02-11 10:45:26 +0530

    Add files via upload

---
+++ 4.apply logical not on non-zero value.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    a=5;
+    int b;
+    b=1;
+    int c;
+    c=-3;
+    
+    printf("%d",!a);
+    printf("\n%d",!b);
+    printf("\n%d",!c);
+    
+    return 0;
+}
