f8c18b8402cbd6a12475200df33367abb3863926
LathikaMahilan <mlathikamlathika78@gmail.com>
2026-02-11 09:22:00 +0530

    Add files via upload

---
+++ 5.decrement a variable twice and print result.c.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+
+int main()
+{
+   int a;
+   a=6;
+   a--;
+   a--;
+   
+   int b;
+   b=2;
+   b--;
+   b--;
+   
+   int c;
+   c=10;
+   c--;
+   c--;
+   
+   printf("%d\n",a);
+   printf("%d\n",b);
+   printf("%d",c);
+   
+    return 0;
+}
