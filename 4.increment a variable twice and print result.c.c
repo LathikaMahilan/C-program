d3824358a51104827db5ba961e426de559e3b7f8
LathikaMahilan <mlathikamlathika78@gmail.com>
2026-02-11 09:18:46 +0530

    Add files via upload

---
+++ 4.increment a variable twice and print result.c.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+
+int main()
+{
+   int a;
+   a=5;
+   a++;
+   a++;
+   
+   int b;
+   b=0;
+   b++;
+   b++;
+   
+   int c;
+   c=10;
+   c++;
+   c++;
+   
+   printf("%d\n",a);
+   printf("%d\n",b);
+   printf("%d",c);
+   
+    return 0;
+}
