80d503db3e7d1e268542eaea768bf9abba82f5e5
LathikaMahilan <mlathikamlathika78@gmail.com>
2026-02-11 09:33:43 +0530

    Add files via upload

---
+++ 2.compare less than .c.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+
+int main()
+{
+   int a;
+   a=5;
+   int b;
+   b=10;
+   int c;
+   c=9;
+   int d;
+   d=2;
+   int e;
+   e=4;
+   int f;
+   f=4;
+   
+   int  x=a<b;
+   int y=c<d;
+   int z=e<f;
+   
+   printf("%d\n",x);
+   printf("%d\n",y);
+   printf("%d",z);
+   
+    return 0;
+}
