a87d7ec4adcb72538a785419a17eecc92fd9ab31
LathikaMahilan <mlathikamlathika78@gmail.com>
2026-02-11 10:37:17 +0530

    Add files via upload

---
+++ 1.use logical and on two non-zero values.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+
+int main()
+{
+   int a,b,c,d,e,f;
+   a=5;
+   b=3;
+   c=1;
+   d=1;
+   e=0;
+   f=5;
+   
+   int x=a&&b;
+   int y=c&&d;
+   int z=e&&f;
+   
+   printf("%d",x);
+   printf("\n%d",y);
+   printf("\n%d",z);
+   
+   
+   
+    
+    return 0;
+}
