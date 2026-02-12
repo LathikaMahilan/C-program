8b1ae2ab64811dddabf7eb44524f14e035c5fd33
LathikaMahilan <mlathikamlathika78@gmail.com>
2026-02-12 15:47:38 +0530

    Add files via upload

---
+++ 20.smallest ternary operator.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main()
+{
+   int a,b;
+   scanf("%d %d",&a,&b);
+   
+   if(a>b){
+       printf("%d is smallest",b);
+   }
+   else{
+       printf("%d is greatest",a);
+   }
+   return 0;
+}
