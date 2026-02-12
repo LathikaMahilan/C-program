523305b2f77ef90563e1b5a2fc11995a306db9eb
LathikaMahilan <mlathikamlathika78@gmail.com>
2026-02-12 15:11:06 +0530

    Add files via upload

---
+++ 18.electricity bill.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main()
+{
+   int units;
+   int bill;
+   scanf("%d",&units);
+   
+   if(units<=100)
+{
+    bill = units * 5;
+}
+else{
+    bill = units * 5;
+}
+    printf("Bill amount = RS. %d ",bill);
+    return 0;
+}
