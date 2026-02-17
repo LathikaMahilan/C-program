1208b6da8a1f67634dbf05a26ce70c99f726efc4
LathikaMahilan <mlathikamlathika78@gmail.com>
2026-02-17 16:18:36 +0530

    Add files via upload

---
+++ 12.for2- fuel price.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n,price;
+    int highest;
+    int highdays=0;
+    scanf("%d",&n);
+    
+    for(int i=0;i<n;i++){
+        scanf("%d",&price);
+        
+        if(price > highest){
+            highest = price;
+        }
+    if(price > 100){
+        highdays++;
+    }
+}
+    printf("highest price: %d\n",highest);
+    printf("high price day: %d\n",highdays);
+
+    return 0;
+}
+    
