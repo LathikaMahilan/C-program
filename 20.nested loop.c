704288e525b1a04e6f1c7b951f716771c431f208
LathikaMahilan <mlathikamlathika78@gmail.com>
2026-03-02 11:56:31 +0530

    Add files via upload

---
+++ 20.nested loop.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, rows = 5;
+
+    
+    for(i = rows; i >= 1; i--) {
+
+        for(j = 1; j <= i; j++)
+            printf("*");
+
+        for(j = 1; j <= 2*(rows-i); j++)
+            printf(" ");
+
+        for(j = 1; j <= i; j++)
+            printf("*");
+
+        printf("\n");
+    }
+
+    
+    for(i = 2; i <= rows; i++) {
+
+        for(j = 1; j <= i; j++)
+            printf("*");
+
+        for(j = 1; j <= 2*(rows-i); j++)
+            printf(" ");
+
+        for(j = 1; j <= i; j++)
+            printf("*");
+
+        printf("\n");
+    }
+
+    return 0;
+}
